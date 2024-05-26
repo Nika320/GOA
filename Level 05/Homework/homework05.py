3c1697837905f85829f1a05309b82186c23e91fc
Nika320 <nikofficialnik7@gmail.com>
2024-05-26 23:02:25 +0200

    Goa

---
+++ Level 05/Homework/homework05.py
@@ -0,0 +1,43 @@
+name = "ნიკა"
+surname = "პატიშვილი"
+age = "12"
+adress = "Stuttgart"
+Food = "Shaurma"
+Color = "Red"
+Car = "Porsche"
+
+
+
+
+# (+)
+print(10 + 9)  
+
+# (-)
+print(9 - 4)  
+
+# (+)
+print(5 + 5)
+
+# (-)
+print(15 - 5)
+
+# (*)
+print(5 * 4)
+
+# (/)
+print(20 / 15)
+
+# (*)
+print(10 * 2)
+
+# (/)
+print(20 / 10)
+
+# (+)
+print(50 + 10)
+
+# (-)
+print(150 - 50)
+
+# (*)
+print(40 * 2)
