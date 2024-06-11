c4eb57b9859e666747b41797d43750df5ac86eda
Nika320 <nikofficialnik7@gmail.com>
2024-06-11 23:35:16 +0200

    GOA

---
+++ Level 09/Classwork/lesson09.py
@@ -0,0 +1,18 @@
+# name = input ('what is your name: ')
+# last_name = input('what is your last name: ')
+# country = input ('where do u live: ')
+
+# print ('my name is ', name, last_name, 'and i live in', country)
+
+
+x = int(input('enter x: '))
+y = int(input('enter y: '))
+
+print(x + y)
+
+
+# type()
+# str()
+
+
+# int()
