2e19fe3eb7c715eb5c1fb0297b93f48e6ab98789
Nika320 <nikofficialnik7@gmail.com>
2024-06-12 19:20:40 +0200

    GOA

---
+++ Level 09/Homework/homework09.py
@@ -0,0 +1,9 @@
+name = input ('what is your name: ')
+last_name = input('what is your last name: ')
+country = input ('where do u live: ')
+Car = input ('Favorite Car: ')
+Color = input ('Favorite Color: ')
+Number = input ('Favorite Number: ')
+
+
+print ( name, last_name, country, Car, Color, Number)
